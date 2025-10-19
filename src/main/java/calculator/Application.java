85d1d5f0b152168ac419322d1b09fcd75f6898c9
JunSeok_Park <pjh203203@naaver.com>
2025-10-19 20:43:18 +0900

    feat(Application): 테스트가 아닌 실제 문자열 입력 및 기능 조립 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        Analyzer analyzer = new Analyzer(input, new Validator());
+        List<Integer> numbers = analyzer.splitString();
+        PlusNumber plusNumber = new PlusNumber(numbers);
+        int result = plusNumber.calculate();
+
+        System.out.println("결과 : " + result);
     }
 }
