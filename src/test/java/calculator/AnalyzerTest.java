64e7f558b0b4406c90b97213a2bbc1bc4de012a0
JunSeok_Park <pjh203203@naaver.com>
2025-10-19 16:32:49 +0900

    teat: 분리된 숫자 0으로 시작할 경우 앞의 0제거 테스트 추가

--- src/test/java/calculator/AnalyzerTest.java
+++ src/test/java/calculator/AnalyzerTest.java
@@ -3,10 +3,12 @@ package calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AnalyzerTest {
 
+    @DisplayName("커스텀 구분자 및 구분자로 분리가 잘되는지")
     @Test
     void analyzeTest() {
         String input = "//;\n1,2:3;4";
@@ -17,4 +19,14 @@ public class AnalyzerTest {
         assertEquals(List.of(testString.split(",")), newInput);
     }
 
+    @DisplayName("0으로 시작하는 숫자가 0을 제거하고 올바르게 분리되는지")
+    @Test
+    void startZeroNumberTest() {
+        String input = "01,05:09";
+        Analyzer analyzer = new Analyzer(input);
+        List<String> newInput = analyzer.splitString();
+
+        String testString = "1,5,9";
+        assertEquals(List.of(testString.split(",")), newInput);
+    }
 }
