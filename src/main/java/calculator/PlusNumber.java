180255bf1ba85af6201c86959ebdf68b2dda0ef7
JunSeok_Park <pjh203203@naaver.com>
2025-10-19 16:39:45 +0900

    refactor(PlusNumber): 문자열을 숫자로 변환 기능 제거

--- src/main/java/calculator/PlusNumber.java
+++ src/main/java/calculator/PlusNumber.java
@@ -1,34 +1,22 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PlusNumber {
-    private final List<String> splitString;
+    private final List<Integer> splitNumber;
 
-    public PlusNumber(List<String> splitString) {
-        this.splitString = splitString;
+    public PlusNumber(List<Integer> splitNumber) {
+        this.splitNumber = splitNumber;
     }
 
     public int calculate() {
-        List<Integer> numbers = convertStringToInteger();
         int result = 0;
 
-        for (Integer number : numbers) {
+        for (Integer number : splitNumber) {
             result += number;
         }
 
         return result;
     }
 
-    private List<Integer> convertStringToInteger() {
-        List<Integer> numbers = new ArrayList<>();
-
-        for (String string : splitString) {
-            int number = Integer.parseInt(string);
-            numbers.add(number);
-        }
-
-        return numbers;
-    }
 }
