7a4fb714a11837b240b7c4483d5772833a833386
JunSeok_Park <pjh203203@naaver.com>
2025-10-19 20:37:34 +0900

    test(ValidatorTest): 임시 변수 제거로 인한 테스트 코드 수정

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -12,18 +12,18 @@ public class ValidatorTest {
     @Test
     void validateOtherCharacter() {
         List<String> splitString = List.of("a", "1", "2");
-        Validator validator = new Validator(splitString);
+        Validator validator = new Validator();
 
-        assertThrows(IllegalArgumentException.class, validator::validateInput);
+        assertThrows(IllegalArgumentException.class, () -> validator.validateInput(splitString));
     }
 
     @DisplayName("커스텀 구분자 및 구분자로 분리된 문자열에 다른 문자가 포함되어있으면 예외 발생")
     @Test
     void validatePlusMinus() {
         List<String> splitString = List.of("+1", "2", "-3");
-        Validator validator = new Validator(splitString);
+        Validator validator = new Validator();
 
-        assertThrows(IllegalArgumentException.class, validator::validateInput);
+        assertThrows(IllegalArgumentException.class, () -> validator.validateInput(splitString));
     }
 
 }
