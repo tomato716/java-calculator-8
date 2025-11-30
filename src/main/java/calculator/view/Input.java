ca0f3b5dc392c0dd13de287b814cfcd17176039d
JunSeok_Park <pjh203203@naaver.com>
2025-11-30 18:22:10 +0900

    feat: Input 클래스 구현

---
+++ src/main/java/calculator/view/Input.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public String inputUser() {
+        return Console.readLine();
+    }
+}
