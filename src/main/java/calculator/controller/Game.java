1a81ed2eba52753b72c74f087455725301729aa7
JunSeok_Park <pjh203203@naaver.com>
2025-11-30 19:29:10 +0900

    feat: 컨트롤러에서 model과 view 조립 구현

--- src/main/java/calculator/controller/Game.java
+++ src/main/java/calculator/controller/Game.java
@@ -1,20 +1,28 @@
 package calculator.controller;
 
+import calculator.model.Delimiter;
+import calculator.model.Numbers;
 import calculator.view.Input;
 import calculator.view.Output;
+import java.util.List;
 
 public class Game {
     private final Input input;
     private final Output output;
+    private final Delimiter delimiter;
 
-    public Game(Input input, Output output) {
+    public Game(Input input, Output output, Delimiter delimiter) {
         this.input = input;
         this.output = output;
+        this.delimiter = delimiter;
     }
 
     public void play() {
         output.printRequestNumber();
         String inputUser = input.inputUser();
+        List<String> splitNumbers = delimiter.separateNumbers(inputUser);
+        Numbers numbers = new Numbers(splitNumbers);
 
+        output.printTotalPlusResult(numbers.calculatePlus());
     }
 }
