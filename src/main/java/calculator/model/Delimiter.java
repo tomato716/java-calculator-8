77c0f9586507c86832e16bc5815de29f3ae0f712
JunSeok_Park <pjh203203@naaver.com>
2025-11-30 23:04:16 +0900

    refactor: 정규식 객체 분리

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -6,13 +6,15 @@ import java.util.regex.Pattern;
 public class Delimiter {
     private static final String COMMA = ",";
     private static final String COLON = ":";
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("^/{2}.\\\\n.*");
 
     public List<String> separateNumbers(String input) {
         if (validateCustomDelimiter(input)) {
             String customDelimiter = findCustomDelimiter(input);
             String removeCustomDelimiterInput = input.substring(5);
 
-            return List.of(removeCustomDelimiterInput.split(COMMA + "|" + COLON + "|" + customDelimiter, -1));
+            return List.of(
+                    removeCustomDelimiterInput.split(COMMA + "|" + COLON + "|" + Pattern.quote(customDelimiter), -1));
         }
         return List.of(input.split(COMMA + "|" + COLON, -1));
     }
@@ -22,7 +24,6 @@ public class Delimiter {
     }
 
     private boolean validateCustomDelimiter(String input) {
-        String regex = "^/{2}.\\\\n.*";
-        return Pattern.matches(regex, input);
+        return CUSTOM_DELIMITER.matcher(input).matches();
     }
 }
