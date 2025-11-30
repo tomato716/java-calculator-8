50e9d858fab914757c97861943171b995e8730c8
JunSeok_Park <pjh203203@naaver.com>
2025-11-30 19:26:55 +0900

    feat: 결과 출력문 구현

--- src/main/java/calculator/view/Output.java
+++ src/main/java/calculator/view/Output.java
@@ -5,4 +5,8 @@ public class Output {
     public void printRequestNumber() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
+
+    public void printTotalPlusResult(int result) {
+        System.out.print("결과 : " + result);
+    }
 }
