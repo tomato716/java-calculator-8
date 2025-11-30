c14b4caa5d6411c15109c90eebf6ba28c8fb6f11
JunSeok_Park <pjh203203@naaver.com>
2025-11-30 19:29:21 +0900

    feat: 컨트롤러 실행 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.controller.Game;
+import calculator.model.Delimiter;
+import calculator.view.Input;
+import calculator.view.Output;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game(new Input(), new Output(), new Delimiter());
+        game.play();
     }
 }
