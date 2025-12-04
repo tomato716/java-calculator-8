aa7f49a4c4b96991649eda8769b222a6c76c9ee2
JunSeok_Park <pjh203203@naaver.com>
2025-12-04 09:52:27 +0900

    refactor: 인덱스 의존성 제거

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -1,35 +1,27 @@
 package calculator.model;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Delimiter {
     private static final String COMMA = ",";
     private static final String COLON = ":";
-    private static final Integer CUSTOM_DELIMITER_START = 2;
-    private static final Integer CUSTOM_DELIMITER_END = 3;
-    private static final Integer CUSTOM_DELIMITER_PREFIX = 5;
-    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("^/{2}.\\\\n.*");
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("^/{2}(.)\\\\n(.*)");
 
     public List<String> split(String input) {
         if (input.isBlank()) {
             return List.of();
         }
-        if (validateCustomDelimiter(input)) {
-            String customDelimiter = findCustomDelimiter(input);
-            String removeCustomDelimiterInput = input.substring(CUSTOM_DELIMITER_PREFIX);
 
-            return List.of(
-                    removeCustomDelimiterInput.split(COMMA + "|" + COLON + "|" + Pattern.quote(customDelimiter), -1));
+        Matcher matcher = CUSTOM_DELIMITER.matcher(input);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String inputInfo = matcher.group(2);
+
+            return List.of(inputInfo.split(COMMA + "|" + COLON + "|" + Pattern.quote(customDelimiter), -1));
         }
         return List.of(input.split(COMMA + "|" + COLON, -1));
-    }
-
-    private String findCustomDelimiter(String input) {
-        return input.substring(CUSTOM_DELIMITER_START, CUSTOM_DELIMITER_END);
-    }
-
-    private boolean validateCustomDelimiter(String input) {
-        return CUSTOM_DELIMITER.matcher(input).matches();
     }
 }
