2fa6e4b327f77bf13f1d264513a337ccd4465d20
JunSeok_Park <pjh203203@naaver.com>
2025-12-06 15:50:35 +0900

    feat: 테스트할 수 있도록 출력문 수정

--- src/main/java/calculator/model/Numbers.java
+++ src/main/java/calculator/model/Numbers.java
@@ -26,7 +26,7 @@ public class Numbers {
         try {
             return Integer.parseInt(inputNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력 문자열이 숫자가 아닙니다. input: " + inputNumber);
+            throw new IllegalArgumentException("[ERROR] 입력 문자열이 숫자가 아닙니다.");
         }
     }
 
